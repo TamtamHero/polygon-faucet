b6256c2a584c3cf525a94e0101d3aba6e8e71384
angela <angela@matic.network>
2020-01-01 03:08:25 +0530

    add test erc20 support

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ const level = require("level");
 
 mkdirp.sync(require("os").homedir() + "/.maticfaucet/exceptions");
 
-const dbExceptions = level(
-  require("os").homedir() + "/.maticfaucet/exceptions"
+const dbEthExceptions = level(
+  require("os").homedir() + "/.maticfaucet/exceptions/eth"
+);
+const dbErc20Exceptions = level(
+    require("os").homedir() + "/.maticfaucet/exceptions/erc20"
 );
 
+const db = {}
+db['maticeth'] = dbEthExceptions
+db['testErc20'] = dbErc20Exceptions
+
 const greylistduration = config.greylistdurationinsec * 1000; // time in ms
+
+const testErc20Artifacts = require('./ChildERC20.json')
+const testErc20TokenAbi = testErc20Artifacts.abi
 
 // check for valid Eth address
 function isAddress(address) {
@@ -23,11 +33,8 @@ function isAddress(address) {
 
 // strip any spaces and add 0x
 function fixaddress(address) {
-    // Strip all spaces
     address = address.replace(" ", "");
-    // Address lowercase
     address = address.toLowerCase();
-    //console.log("Fix address", address);
     if (!strStartsWith(address, "0x")) {
       return "0x" + address;
     }
@@ -39,7 +46,7 @@ function strStartsWith(str, prefix) {
     return str.indexOf(prefix) === 0;
 }
 
-let web3Objects = [];
+let web3Objects = {};
 
 for (let network in config.networks) {
     console.log(network)
@@ -50,28 +57,37 @@ for (let network in config.networks) {
     console.log('adding key')
     web3.eth.accounts.wallet.add (currentNetwork.privateKey)
     console.log('wallet addr=', web3.eth.accounts.wallet[0].address)
-    web3Objects.push(web3)
+    web3Objects[network] = web3
     console.log('---')
 }
 
-function getBalance(web3) {
+function getEthBalance(web3) {
     return (web3.eth.getBalance(web3.eth.accounts.wallet[0].address))
+}
+function getTestErc20Balance(web3) {
+    let host = web3.currentProvider.host
+    let network = host.replace("https://", "").replace(".matic.network", "")
+    let tokenContract = new web3.eth.Contract(testErc20TokenAbi,config.networks[network].tokens.testErc20.tokenAddress)
+    return tokenContract.methods.balanceOf(web3.eth.accounts.wallet[0].address).call()
 }
 
 async function getFaucetBalance() {
     let balances = [];
     for (let obj in web3Objects) {
         let web3 = web3Objects[obj]
-        const r = await getBalance(web3)
+        
+        let rEth = await getEthBalance(web3)
+        let rTest20 = await getTestErc20Balance(web3)
+
         balances.push({
-            "network": web3.currentProvider.host,
+            "network": web3.currentProvider.host.replace("https://", "").replace(".matic.network", ""),
             "account": web3.eth.accounts.wallet[0].address,
-            "balance": web3.utils.fromWei(r, 'ether')
+            "balanceEth": web3.utils.fromWei(rEth, 'ether'),
+            "balanceTestErc20": web3.utils.fromWei(rTest20, 'ether') 
         });
     }
     return balances
 }
-// getFaucetBalance()
 
 // start webserver
 app.listen(config.httpport, function() {
@@ -83,11 +99,9 @@ app.use(cors());
 //frontend app serving directory
 app.use(express.static("static/build"));
 
-
 app.get('/info', function(req, res) {
     var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     console.log("client IP=", ip);
-
     getFaucetBalance().then((r) => {
         res.status(200).json({
             checkfreqinsec: config.checkfreqinsec,
@@ -95,32 +109,31 @@ app.get('/info', function(req, res) {
             balances: r
         })
     })
-
 })
 
-function getException(address) {
-    return new Promise((resolve, reject) => {
-        dbExceptions.get(address, function(err, value) {
+function getException(address, token) {
+    return new Promise ((resolve, reject) => {
+        db[token].get(address, function(err, value) {
             if (err) {
                 if (err.notFound) {
-                    return resolve();
+                    return resolve()
                 }
                 return reject(err)
             }
-            value = JSON.parse(value);
-            resolve(value);
+            value = JSON.parse(value)
+            resolve(value)
         })
     })
 }
 
-function setException(address, reason){
+function setException(address, token){
     console.log("adding", address, "to greylist")
     return new Promise((resolve, reject) => {
-        dbExceptions.put(
+        db[token].put(
             address, 
             JSON.stringify({
                 created: Date.now(),
-                reason: reason, 
+                reason: 'greylist', 
                 address: address
             }),
             function(err) {
@@ -133,35 +146,33 @@ function setException(address, reason){
     })
 }
 
-function cleanupException() {
-    var stream = dbExceptions.createReadStream({
+function cleanupExceptions(token) {
+    var stream = db[token].createReadStream({
         keys: true,
         values: true
     }).on("data", item => {
         const value = JSON.parse(item.value);
-        if(value.reason === "greylist") {
-            if(value.created < Date.now() - greylistduration) {
-                dbExceptions.del(item.key, err => {
-                    console.log("removed ", item.key, "from greylist.");
-                })
-            }
+        if(value.created < Date.now() - greylistduration) {
+            db[token].del(item.key, err => {
+                console.log("removed ", item.key, "from greylist.");
+            })
         }
     })
 }
 
 // exception monitor
 setInterval(() => {
-    cleanupException();
-}, config.checkfreqinsec * 1000);
+    cleanupExceptions('maticeth')
+    cleanupExceptions('testErc20')
+}, config.checkfreqinsec * 100);
 
-app.get("/testnet2/maticeth/:address", function(req, res) {
+app.get("/:network/:token/:address", function(req, res) {
     var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
     console.log("client IP=", ip);
-
-    var address = req.params.address
-    var amount = config.networks.testnet2.tokens.maticeth.payoutamountinether
-    var network = 0
-
+    let network = req.params.network 
+    let token = req.params.token 
+    let address = req.params.address 
+    let amount = config.networks[network].tokens[token].payoutamount
     if (!isAddress(fixaddress(address))) {
         // invalid addr
         console.log("INVALID ADDR. 400")
@@ -169,40 +180,7 @@ app.get("/testnet2/maticeth/:address", function(req, res) {
             msg: "invalid address."
         })
     }
-    
-    startTransfer(address, ip, amount, network).then((r) => {
-        // successful transaction
-        console.log("OK. 200")
-        return res.status(200).json({
-            hash: r
-        });
-    }).catch(e => {
-        // either tx error/ greylisted
-        console.log("ERROR:500")
-        return res.status(500).json({
-            err: e
-        });
-    })
-
-})
-
-app.get("/testnet3/maticeth/:address", function(req, res) {
-    var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("client IP=", ip);
-
-    var address = req.params.address
-    var amount = config.networks.testnet3.tokens.maticeth.payoutamountinether
-    var network = 1
-
-    if (!isAddress(fixaddress(address))) {
-        // invalid addr
-        console.log("INVALID ADDR. 400")
-        return res.status(400).json({
-            msg: "invalid address."
-        })
-    }
-    
-    startTransfer(address, ip, amount, network).then((r) => {
+    startTransfer(ip, address, token, amount, network).then((r) => {
         // successful transaction
         console.log("OK. 200")
         return res.status(200).json({
@@ -217,112 +195,42 @@ app.get("/testnet3/maticeth/:address", function(req, res) {
     })
 })
 
-app.get("/alpha/maticeth/:address", function(req, res) {
-    var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("client IP=", ip);
+async function startTransfer(ip, address, token, amount, network) {
+    let addressException = await getException(address, token)
+    let ipException = await getException(ip, token)
 
-    var address = req.params.address
-    var amount = config.networks.testnet3.tokens.maticeth.payoutamountinether
-    var network = 2
-
-    if (!isAddress(fixaddress(address))) {
-        // invalid addr
-        console.log("INVALID ADDR. 400")
-        return res.status(400).json({
-            msg: "invalid address."
-        })
+    let exception = addressException || ipException
+    
+    if (exception) {
+        console.log(exception.address, "is on the greylist");
+        var values = {
+            address: exception.address,
+            message: "you are greylisted",
+            duration: (exception.created + greylistduration) - Date.now()
+        }
+        return Promise.reject(values)
     }
     
-    startTransfer(address, ip, amount, network).then((r) => {
-        // successful transaction
-        console.log("OK. 200")
-        return res.status(200).json({
-            hash: r
-        });
-    }).catch(e => {
-        // either tx error/ greylisted
-        console.log("ERROR:500")
-        return res.status(500).json({
-            err: e
-        });
-    })
-})
-
-app.get("/beta2/maticeth/:address", function(req, res) {
-    var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("client IP=", ip);
-
-    var address = req.params.address
-    var amount = config.networks.testnet3.tokens.maticeth.payoutamountinether
-    var network = 3
-
-    if (!isAddress(fixaddress(address))) {
-        // invalid addr
-        console.log("INVALID ADDR. 400")
-        return res.status(400).json({
-            msg: "invalid address."
-        })
-    }
+    let receipt = await _startTransfer(address, token, amount, network)
     
-    startTransfer(address, ip, amount, network).then((r) => {
-        // successful transaction
-        console.log("OK. 200")
-        return res.status(200).json({
-            hash: r
-        });
-    }).catch(e => {
-        // either tx error/ greylisted
-        console.log("ERROR:500")
-        return res.status(500).json({
-            err: e
-        });
-    })
-})
-
-function startTransfer(address, ip, amount, network) {
-    return new Promise ((resolve, reject) => {
-        Promise.all([getException(address), getException(ip)]).then(
-            ([addressException, ipException]) => {
-                var exception = addressException || ipException;
+    await setException(address, token)
+    await setException(ip, token)
     
-                if (exception) {
-                    console.log(exception.address, "is on the greylist");
-                    var values = {
-                        address: exception.address,
-                        message: "you are greylisted",
-                        duration: (exception.created + greylistduration) - Date.now()
-                    }
-                    reject(values);
-                } else {
-                    // transfer funds
-                    console.log('transferring',amount, 'Matic ETH to', address)
-                    transferEth(address, amount, network).then((r) => {
-                        Promise.all([setException(ip, 'greylist'), setException(address, 'greylist')]).then(() => {
-                            resolve(r)
-                        })
-                    }).catch(e => {
-                        reject(e)
-                    })
-                }
-            }
-        )
-    })
+    return receipt
+}
+
+async function _startTransfer(address, token, amount, network) {
+    if (token === 'maticeth') return transferEth(address, amount, network)
+    if (token === 'testErc20') return transferTestErc20(address, amount, network)
 }
 
 async function transferEth(_to, _amount, network) {
     console.log('---start tx---')
-    let web3
-    web3 = web3Objects[network]
+    let web3 = web3Objects[network]
     let _from = web3.eth.accounts.wallet[0].address
-
     let _gasPrice = await web3.eth.getGasPrice();
-    
     console.log("gasprice is ", _gasPrice);
-    
-    console.log('transferring on',web3.currentProvider.host)
-
-    var amt = _amount * 1e18;
-
+    let amt = (_amount * Math.pow(10, 18)).toString()
     var options = {
         from: _from,
         to: _to,
@@ -330,13 +238,46 @@ async function transferEth(_to, _amount, network) {
         gas: 314150,
         gasPrice: _gasPrice
     }
-
-    console.log ('transferring', amt, 'wei from', _from, 'to', _to);
-
     console.log(options);
-
-    const result = await web3.eth.sendTransaction(options);
-    console.log('transfer successful!', result.transactionHash)
+    let r = await web3.eth.sendTransaction(options)
+                  .on('receipt', (receipt) => {
+                      console.log('transfer successful!', receipt.transactionHash)
+                  })
+                  .on('error', (err) => {
+                      return Promise.reject(err);
+                  })
     console.log('---end tx---')
-    return result.transactionHash;
+    return Promise.resolve(r.transactionHash);
+}
+
+async function transferTestErc20(_to, _amount, network) {
+    console.log('---start tx---')
+    let web3 = web3Objects[network]
+    let _from = web3.eth.accounts.wallet[0].address
+    let _gasPrice = await web3.eth.getGasPrice()
+    console.log('gas price is', _gasPrice)
+
+    let tokenAddress = config.networks[network].tokens.testErc20.tokenAddress
+    let tokenContract = new web3.eth.Contract(testErc20TokenAbi, tokenAddress)
+
+    let decimals = await tokenContract.methods.decimals().call()
+    let amt = (_amount * Math.pow(10, decimals)).toString()
+    let options = {
+        from: _from,
+        to: _to,
+        gas: 314150,
+        gasPrice: _gasPrice
+    }
+    console.log(options);
+    let r = await tokenContract.methods.transfer(_to, amt)
+                               .send(options)
+                               .on('receipt', (receipt) => {
+                                   console.log(receipt.transactionHash)
+                               })
+                               .on('error', (err) => {
+                                   return Promise.reject(err)
+                               })
+
+    console.log('---end tx---')
+    return Promise.resolve(r.transactionHash)
 }
