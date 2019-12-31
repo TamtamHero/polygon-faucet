3492311f4344fbf95a0805c21f8ccfdc3bd81b04
angela <angela@matic.network>
2020-01-01 03:09:08 +0530

    remove test

--- test.js
+++
@@ -1,5 +0,0 @@
-const Web3 = require('web3')
-const web3 = new Web3('https://testnet2.matic.network')
-let amount = 1
-console.log('hey', web3.utils.toWei(amount.toString(),'ether'))
-// let amt = web3.utils.toWei(amount, 'ether')
